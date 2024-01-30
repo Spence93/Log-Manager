e97a3bdb60b1c5d51ec7559debf15002fb3ac1e6
Matt Spencer <spencermatt93@gmail.com>
2024-01-30 19:48:01 +0000

    Created single input function, removed exit_log funciton

--- log_manager.py
+++ log_manager.py
@@ -19,130 +19,79 @@ def branch_list():
 
 def list_logs(clean_list):
     print("\nLondon Branch Logs: ")
-    for i, dates in enumerate(clean_list, 1):  
-        print(i,": ", dates[0], dates[2] )
+    for i, dates in enumerate(clean_list, 1):
+        print(i, ": ", dates[0], dates[2])
     print("Please choose which log you wish to open")
-    print("Enter 'n' to return to previous menu")    
+    print("Enter 'n' to return to previous menu")
 
 
-# merge input functions into one?
-def first_input():
+def user_input_validation(prompt):
     while True:
-        input_one = input("--> ")
-        if input_one.isnumeric():
-            input_one = int(input_one)
-            if input_one > 0 and input_one <= len(menu_list):
+        user_input = input(prompt)
+        if user_input.isnumeric():
+            user_input = int(user_input)
+            if user_input > 0 and user_input <= len(log_files):
                 break
             else:
                 print("Please enter a valid option")
                 continue
+        elif user_input.lower() == "n":
+            return user_input
         else:
             print("Please enter a valid option")
             continue
-    return input_one        
+    return user_input
 
-
-def branch_input():
-    while True:
-        input_two = input("--> ")
-        if input_two.isnumeric():
-            input_two = int(input_two)
-            if input_two > 0 and input_two <= len(log_files):
-                break
-            else:
-                print("Please enter a valid option")
-                continue            
-        elif input_two.lower() == "n":
-            return input_two
-        else:
-            print("Please enter a valid option")
-            continue
-    return input_two 
-
-
-def choose_log():
-    while True:
-        input_three = input("--> ")
-        if input_three.isnumeric():
-            input_three = int(input_three)
-            if input_three > 0 and input_three <= len(clean):
-                break
-            else:
-                print("Please enter a valid option")
-                continue
-        elif input_three.lower() == "n":
-            return input_three            
-        else:
-            print("Please enter a valid option")
-            continue
-    return input_three
-
-
-def exit_log():
-    while True:
-        input_four = input("-->: ")
-        if input_four.lower() == "y":
-            break            
-        else:
-            sub_list = True
-            return sub_list        
 
 def read_london():
     with open(london, "r") as file:
         london_log = file.readlines()
         return london_log
-    
+
 
 def list_clean(list):
     new_list = []
     for i in list:
-        x = i.strip( )
-        new_list.append(x.split("|"))     
-    return new_list  
+        x = i.strip()
+        new_list.append(x.split("|"))
+    return new_list
 
 
 def print_log(open_log):
-        open_log -= 1
-        print(f"\nDATE: {clean[open_log][0]}\nSTATUS: {clean[open_log][1]}\nORDER NO: {clean[open_log][2]}\nBRANCH: {clean[open_log][3]}\nDEPARTMENT: {clean[open_log][4]}")
-        print("\nType 'y' to exit current log")
-
+    open_log -= 1
+    print(f"\nDATE: {clean[open_log][0]}\nSTATUS: {clean[open_log][1]}\nORDER NO: {
+          clean[open_log][2]}\nBRANCH: {clean[open_log][3]}\nDEPARTMENT: {clean[open_log][4]}")
 
 
 main_menu = False
 menu_list = ["View shipping Log List",
-             "Search for a Shipping Log", 
+             "Search for a Shipping Log",
              "Exit Program"]
 log_files = ["London.txt", "Manchester.txt", "Glasgow.txt"]
-london = "London.txt" 
+london = "London.txt"
 sub_list = False
 logs = False
 # add variable to single function to select branch txt fille !!
 
 
-
-
-
-
-
-
-# Main menu and an input function for the user to select an option 
+# Main menu and an input function for the user to select an option
 # sub_list var to res
 while (not main_menu):
     main_screen()
-    main_input = first_input()
+    main_input = user_input_validation("--> ")
 
     # add while loop to take back to branch menu??
 
     # If statement to check user input and move to the Branch List menu
-    # Another input function for user to select which Branch's log they 
+    # Another input function for user to select which Branch's log they
     # would like to acess
     if main_input == 1:
-        logs = False    
-        while (not logs):    
-            # This funciton prints out the branch list and we get the user choice for 
+        logs = False
+        while (not logs):
+            # This funciton prints out the branch list and we get the user choice for
             # the menu stored in log_input
             branch_list()
-            log_input = branch_input()
+            log_input = user_input_validation("--> ")
 
             # Option 1 run opens and cleans the London.txt file
             if log_input == 1:
@@ -150,28 +99,25 @@ while (not main_menu):
                 print_london = read_london()
                 # cleans the list of unwanted chars and creates 2D list
                 clean = list_clean(print_london)
-                sub_list = False 
+                sub_list = False
 
                 # While loop used to display the London branch logs, and give the user a choice
                 # of which log they wish to view in full
                 while (not sub_list):
                     log_dates = list_logs(clean)
-                    open_log = choose_log()
+                    open_log = user_input_validation("--> ")
 
-                    # uses the input stored in open_log to open the chosen log 
+                    # uses the input stored in open_log to open the chosen log
                     if open_log != "n":
                         print_log(open_log)
-                        exit_log()                  
-                    else:    
+                        if input("Enter any key to close "):
+                            break
+                    else:
                         sub_list = True
-
-
 
             # New York option 2
             elif log_input == 2:
                 pass
-
-
 
             # Paris option 3
             elif log_input == 3:
@@ -181,25 +127,8 @@ while (not main_menu):
             elif log_input == "n":
                 logs = True
 
-
-                # # While loop used to display the London branch logs, and give the user a choice
-                # # of which log they wish to view in full
-                # while (not sub_list):
-                #     log_dates = list_logs(clean)
-                #     open_log = choose_log()
-
-                #     # usese the input stored in open_log to open the chosen log 
-                #     if open_log != "n":
-                #         print_log(open_log)
-                #         exit_log() 
-
-                #     # Allows user to exit from this menu (loop) and go back to main menu                 
-                #     else:    
-                #         sub_list = True
-                    
-
     elif main_input == 2:
-        pass       
+        pass
 
     elif main_input == 3:
         print("Exiting program, Goodbye")
