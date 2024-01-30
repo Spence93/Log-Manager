0c62c92ebb5415fdceee4ae0763008eab7e074e1
Matt Spencer <spencermatt93@gmail.com>
2024-01-30 21:40:35 +0000

    reduced code and functions

--- log_manager.py
+++ log_manager.py
@@ -30,7 +30,7 @@ def user_input_validation(prompt):
         user_input = input(prompt)
         if user_input.isnumeric():
             user_input = int(user_input)
-            if user_input > 0 and user_input <= len(log_files):
+            if user_input > 0 <= len(log_files):  # changed 'and user_input'
                 break
             else:
                 print("Please enter a valid option")
@@ -42,11 +42,13 @@ def user_input_validation(prompt):
             continue
     return user_input
 
+# rename as now multiuse
 
-def read_london():
-    with open(london, "r") as file:
-        london_log = file.readlines()
-        return london_log
+
+def read_log(city):
+    with open(city, "r") as file:
+        city_log = file.readlines()
+        return city_log
 
 
 def list_clean(list):
@@ -57,74 +59,68 @@ def list_clean(list):
     return new_list
 
 
-def print_log(open_log):
-    open_log -= 1
-    print(f"\nDATE: {clean[open_log][0]}\nSTATUS: {clean[open_log][1]}\nORDER NO: {
-          clean[open_log][2]}\nBRANCH: {clean[open_log][3]}\nDEPARTMENT: {clean[open_log][4]}")
+def print_log(city_log):
+    """
+    The function `print_log` takes a city log as input and prints the corresponding date, status, order
+    number, branch, and department.
+
+    :param city_log: The parameter `city_log` is used to specify the index of the log entry in the
+    `clean` list that we want to print
+    """
+    city_log -= 1
+    print(f"\nDATE: {clean[city_log][0]}\nSTATUS: {clean[city_log][1]}\nORDER NO: {
+          clean[city_log][2]}\nBRANCH: {clean[city_log][3]}\nDEPARTMENT: {clean[city_log][4]}")
 
 
-main_menu = False
 menu_list = ["View shipping Log List",
              "Search for a Shipping Log",
              "Exit Program"]
 log_files = ["London.txt", "Manchester.txt", "Glasgow.txt"]
-london = "London.txt"
+main_menu = False
 sub_list = False
 logs = False
-# add variable to single function to select branch txt fille !!
 
 
 # Main menu and an input function for the user to select an option
-# sub_list var to res
 while (not main_menu):
     main_screen()
     main_input = user_input_validation("--> ")
 
-    # add while loop to take back to branch menu??
-
-    # If statement to check user input and move to the Branch List menu
-    # Another input function for user to select which Branch's log they
-    # would like to acess
     if main_input == 1:
+
         logs = False
         while (not logs):
-            # This funciton prints out the branch list and we get the user choice for
-            # the menu stored in log_input
             branch_list()
-            log_input = user_input_validation("--> ")
+            branch_input = user_input_validation("--> ")
 
-            # Option 1 run opens and cleans the London.txt file
-            if log_input == 1:
-                # reads the lond.txt file as readlines() - stores in list
-                print_london = read_london()
+            if branch_input == 1:
+                print_london = read_log(log_files[0])
                 # cleans the list of unwanted chars and creates 2D list
                 clean = list_clean(print_london)
-                sub_list = False
 
-                # While loop used to display the London branch logs, and give the user a choice
-                # of which log they wish to view in full
+                sub_list = False
                 while (not sub_list):
                     log_dates = list_logs(clean)
-                    open_log = user_input_validation("--> ")
+                    london_logs = user_input_validation("--> ")
 
-                    # uses the input stored in open_log to open the chosen log
-                    if open_log != "n":
-                        print_log(open_log)
+                    # uses the input stored in london_logs to open the chosen log
+                    if london_logs != "n":
+                        print_log(london_logs)
                         if input("Enter any key to close "):
-                            break
+                            continue
                     else:
                         sub_list = True
 
             # New York option 2
-            elif log_input == 2:
+            elif branch_input == 2:
                 pass
 
             # Paris option 3
-            elif log_input == 3:
+            elif branch_input == 3:
                 pass
 
             # Exit
-            elif log_input == "n":
+            elif branch_input == "n":
                 logs = True
 
     elif main_input == 2:
