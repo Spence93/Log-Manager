7ff7958af7647591288e4ef0c85299249ba91498
Matt Spencer <spencermatt93@gmail.com>
2024-02-26 11:22:56 +0000

    Fixed issue and added multi line strings as required

--- log_manager.py
+++ log_manager.py
@@ -52,19 +52,6 @@ def list_clean(list):
     return new_list
 
 
-def print_log(city_log, clean_list):
-    """
-    The function `print_log` takes a city log as input and prints the corresponding date, status, order
-    number, branch, and department.
-
-    :param city_log: The parameter `city_log` is used to specify the index of the log entry in the
-    `clean` list that we want to print
-    """
-    city_log -= 1
-    print(f"\nDATE: {clean_list[city_log][0]}\nSTATUS: {clean_list[city_log][1]}\nORDER NO: {
-          clean_list[city_log][2]}\nBRANCH: {clean_list[city_log][3]}\nDEPARTMENT: {clean_list[city_log][4]}")
-
-
 def find_log(clean_list):
     while True:
         temp_list = []
@@ -92,10 +79,24 @@ def find_log(clean_list):
         return temp_list
 
 
+#  ----------------- Print Functions --------------------
+def print_log(city_log, clean_list):
+    """
+    The function `print_log` takes a city log as input and prints the corresponding date, status, order
+    number, branch, and department.
+
+    :param city_log: The parameter `city_log` is used to specify the index of the log entry in the
+    `clean` list that we want to print
+    """
+    city_log -= 1
+    print(f"""\nDATE: {clean_list[city_log][0]}\nSTATUS: {clean_list[city_log][1]}\nORDER NO: {
+          clean_list[city_log][2]}\nBRANCH: {clean_list[city_log][3]}\nDEPARTMENT: {clean_list[city_log][4]}""")
+
+
 def print_search(search_list):
     search_input = 0
-    print(f"\nDATE:\t\t {search_list[search_input][0]}\nSTATUS:\t\t{search_list[search_input][1]}\nORDER NO:\t{
-          search_list[search_input][2]}\nBRANCH:\t\t{search_list[search_input][3]}\nDEPARTMENT:\t{search_list[search_input][4]}")
+    print(f"""\nDATE:\t\t {search_list[search_input][0]}\nSTATUS:\t\t{search_list[search_input][1]}\nORDER NO:\t{
+          search_list[search_input][2]}\nBRANCH:\t\t{search_list[search_input][3]}\nDEPARTMENT:\t{search_list[search_input][4]}""")
 
 
 home_page = ("\nHyperion Enterprises") + ("\nShipping Log Manager")
@@ -111,22 +112,24 @@ main_menu = False
 while (not main_menu):
     main_input = menu_inputs(home_page, log_files, menu_list)
 
+    # View shopping Logs from a list
     if main_input == 1:
         while True:
+            # Prints branch options, and returns users input
             branch_input = menu_inputs(branch, log_files, branches)
             if branch_input == "n":
                 break
 
+            # If user selects view London Logs
             if branch_input == 1:
                 while True:
                     # opens file to read and store as list
                     print_london = read_log(log_files[0])
                     # cleans the list of unwanted chars and creates 2D list
                     clean_london = list_clean(print_london)
-                    london_log_input = list_logs(
+                    london_log_input = list_logs(           # Lists all logs in the london.txt file, returns users choice to view
                         "London", clean_london, log_files)
-
-                    if london_log_input != "n":
+                    if london_log_input != "n":              # if user selects a valid option - the log is printed out
                         print_log(london_log_input, clean_london)
                         if input("\nPress 'n' to return "):
                             continue
@@ -157,16 +160,20 @@ while (not main_menu):
                         if input("\nPress 'n' to return"):
                             continue
                     break
-
+    # User search function for a specific log by branch and order number.
     elif main_input == 2:
         while True:
+            # Prints branch otions, and returns users choice
             s_input = menu_inputs(branch, log_files, branches)
-            if s_input == "n":
+            if s_input == "n":      # break loop to return to previous menu
                 break
+            # User input indexes the correct Branches .txt file for reading
             search_branch = read_log(log_files[s_input - 1])
+            # branch.txt file is cleaned into the desired format
             search_clean = list_clean(search_branch)
 
             while True:
+                # user input to search for the logs Order number
                 search_result = find_log(search_clean)
                 if search_result != "n":
                     print_search(search_result)
